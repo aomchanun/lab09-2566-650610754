4fe01c9267cd09a4c93b5a9d7add7b45aa30e893
aomchanun <omsinjah@gmail.com>
2023-07-20 17:56:34 +0700

    completed

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -11,7 +11,7 @@ export const Task = ({
 
   const doneBtnOnClick = () => {
     toggleDoneTaskFunc(id);
-    completed == !completed;
+    completed = !completed;
   };
 
   const resultClassName = completed ? "text-decoration-line-through" : "";
